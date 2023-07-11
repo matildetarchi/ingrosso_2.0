6ed3afcc01fb172cdc146e8fd1b148c29048d4d3
matildetarchi <matilde.tarchi@stud.unifi.it>
2023-07-11 11:07:17 +0200

    review Order class

--- Order.cpp
+++ Order.cpp
@@ -5,11 +5,10 @@
 #include "Order.h"
 
 
-Order::Order(const string &s, const string &d, const string &u_c, int id_p){
+Order::Order(const string &s, Date* d, const string &u_c, int id_p){
 
-    //TODO devo mettere anche product?
+    //TODO devo mettere anche product e date?
     status = s;
-    date_order = d;
     username_client = u_c;
     id_s_product = id_p;
 
--- Order.h
+++ Order.h
@@ -6,11 +6,12 @@
 #define INGROSSO_ONLINE_ORDER_H
 
 #include "OrderProduct.h"
+#include "Date.h"
 
 class Order {
 public:
 
-    Order(const string &s, const string &d, const string &u_c, int id_s_p);
+    Order(const string &s, Date* d, const string &u_c, int id_s_p);
 
     ~Order();
 
@@ -41,12 +42,12 @@ public:
 
     //TODO è da fare qui set prod?
 
-    const string &get_date() {
-        return date_order;
+    Date* get_date() {
+        return date;
     }
 
-    void set_date(string date_o) {
-        this->date_order=date_o;
+    void set_date(Date d) {
+        date = &d;
     }
 
     const string &get_us_client() {
@@ -60,11 +61,11 @@ public:
 
 private:
 
+    Date* date;
     Product* prod;
     int num_prod;
     int id_s_product;
     string status;
-    string date_order;
     string username_client;
 
 };
